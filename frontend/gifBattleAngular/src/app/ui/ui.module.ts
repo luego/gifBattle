483316557823ee231766fcb15ba8b1a5fd243e0a
Luis Eduardo Gonzalez <lgonzalez@tecnoesw.com>
2019-10-28 17:40:25 -0500

    edit: adding component layout

--- frontend/gifBattleAngular/src/app/ui/ui.module.ts
+++ frontend/gifBattleAngular/src/app/ui/ui.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import { HeaderComponent } from './header/header.component';
@@ -6,6 +6,8 @@ import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [LayoutComponent, HeaderComponent, FooterComponent],
-  imports: [CommonModule]
+  imports: [CommonModule],
+  exports: [LayoutComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class UiModule {}
