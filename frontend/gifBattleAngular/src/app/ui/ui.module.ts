e62e51b4cb6a8bd79f154e6453c676b5f43b4177
Luis Eduardo Gonzalez <luego1986@gmail.com>
2019-11-05 11:46:10 -0500

    Fix content

--- frontend/gifBattleAngular/src/app/ui/ui.module.ts
+++ frontend/gifBattleAngular/src/app/ui/ui.module.ts
@@ -10,10 +10,25 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [LayoutComponent, HeaderComponent, FooterComponent, NavigationComponent],
-  imports: [CommonModule, LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule],
+  declarations: [
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavigationComponent
+  ],
+  imports: [
+    CommonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    RouterModule
+  ],
   exports: [LayoutComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
